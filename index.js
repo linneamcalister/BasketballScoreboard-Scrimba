072b476df8666d1d981b3fb2a34417edef8cbee3
linneamcalister <126209884+linneamcalister@users.noreply.github.com>
2023-04-15 13:33:23 -0400

    add window functions

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ function incrementByOneA () {
     scoreA += 1
     homeScore.textContent = scoreA
 }
-
+window.incrementByOneA = incrementByOneA
 function incrementByTwoA () {
     scoreA += 2
     homeScore.textContent = scoreA
 }
-
+window.incrementByTwoA = incrementByTwoA
 function incrementByThreeA () {
     scoreA += 3
     homeScore.textContent = scoreA
 }
-
+window.incrementByThreeA = incrementByThreeA
 
 let guestScore = document.getElementById("guestscore")
 let scoreB = 0
@@ -24,20 +24,21 @@ function incrementByOneB () {
     scoreB += 1
     guestScore.textContent = scoreB
 }
-
+window.incrementByOneB = incrementByOneB
 function incrementByTwoB () {
     scoreB += 2
     guestScore.textContent = scoreB
 }
-
+window.incrementByTwoB = incrementByTwoB
 function incrementByThreeB () {
     scoreB += 3
     guestScore.textContent = scoreB
 }
-
+window.incrementByThreeB = incrementByThreeB
 function setToZero () {
     scoreA = 0
     homeScore.textContent = scoreA
     scoreB = 0
     guestScore.textContent = scoreB
 }
+window.setToZero = setToZero
